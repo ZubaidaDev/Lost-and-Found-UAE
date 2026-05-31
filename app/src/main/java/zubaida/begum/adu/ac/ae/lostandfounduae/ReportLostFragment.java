cb851e82e2df199fb4a55e8a5d9cf1be4c4e26ed
almess3bi <almess3bi@gmail.com>
2026-05-31 20:55:00 +0400

    Update ReportLostFragment.java

--- app/src/main/java/zubaida/begum/adu/ac/ae/lostandfounduae/ReportLostFragment.java
+++ app/src/main/java/zubaida/begum/adu/ac/ae/lostandfounduae/ReportLostFragment.java
@@ -1,5 +1,5 @@
 package zubaida.begum.adu.ac.ae.lostandfounduae;
-
+private FusedLocationProviderClient fusedLocationClient;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
